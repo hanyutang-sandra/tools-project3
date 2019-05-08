f7a9a8b7e91887bfbdcbfa516b776619c03c5ee7
Hanyu <hanyutang.sandra@gmail.com>
2019-05-08 06:26:33 -0400

    Updates

---
+++ precache-manifest.17099ff107fc0c2e7df9814a493e9b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3357feb3e2cb5697c499e0523af4294a",
+    "url": "/tools-project3/index.html"
+  },
+  {
+    "revision": "7bcc7c74ea53ac8a122a",
+    "url": "/tools-project3/static/js/2.0dea6780.chunk.js"
+  },
+  {
+    "revision": "d7b8f8d72afc8c742f9f",
+    "url": "/tools-project3/static/js/main.c021ad0a.chunk.js"
+  },
+  {
+    "revision": "bc9595af37abcf4900e7",
+    "url": "/tools-project3/static/js/runtime~main.3e987044.js"
+  },
+  {
+    "revision": "bd883ae7d224bec34f6463a1c10172c6",
+    "url": "/tools-project3/static/media/end.bd883ae7.png"
+  },
+  {
+    "revision": "808c43138fa30be9ae740dee184f5aab",
+    "url": "/tools-project3/static/media/start.808c4313.png"
+  }
+]);
